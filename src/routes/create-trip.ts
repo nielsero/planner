172b95adb9266a3a9ea0eec8f76e5aef3d6b2e47
nielsero <niels.cpp@gmail.com>
2024-07-24 00:29:42 +0200

    feat: Send email to other participants on create

--- src/routes/create-trip.ts
+++ src/routes/create-trip.ts
@@ -17,12 +17,19 @@ export async function createTrip(app: FastifyInstance) {
           ends_at: z.coerce.date(),
           owner_name: z.string(),
           owner_email: z.string().email(),
+          emails_to_invite: z.array(z.string().email()),
         }),
       },
     },
     async (request) => {
-      const { destination, starts_at, ends_at, owner_name, owner_email } =
-        request.body;
+      const {
+        destination,
+        starts_at,
+        ends_at,
+        owner_name,
+        owner_email,
+        emails_to_invite,
+      } = request.body;
 
       if (dayjs(starts_at).isBefore(new Date())) {
         throw new Error("Invalid trip start date");
@@ -38,11 +45,18 @@ export async function createTrip(app: FastifyInstance) {
           starts_at,
           ends_at,
           participants: {
-            create: {
-              name: owner_name,
-              email: owner_email,
-              is_owner: true,
-              is_confirmed: true,
+            createMany: {
+              data: [
+                {
+                  name: owner_name,
+                  email: owner_email,
+                  is_owner: true,
+                  is_confirmed: true,
+                },
+                ...emails_to_invite.map((email) => {
+                  return { email };
+                }),
+              ],
             },
           },
         },
