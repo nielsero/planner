754adf150e9050a2d9d9e5399f04c8784eee419c
nielsero <niels.cpp@gmail.com>
2024-07-24 03:08:37 +0200

    refactor: Delete redundant files

--- src/lib/dayjs.ts
+++
@@ -1,8 +0,0 @@
-import dayjs from "dayjs";
-import localizedFormat from "dayjs/plugin/localizedFormat";
-import "dayjs/locale/pt-br";
-
-dayjs.locale("pt-br");
-dayjs.extend(localizedFormat);
-
-export { dayjs };
