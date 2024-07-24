906744b1ba4107c5a4d11c749cfdaa467109da4c
nielsero <niels.cpp@gmail.com>
2024-07-24 02:53:58 +0200

    feat: Add get links

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ import { confirmParticipant } from "./routes/confirm-participant";
 import { createActivity } from "./routes/create-activity";
 import { getActivities } from "./routes/get-activities";
 import { createLink } from "./routes/create-link";
+import { getLinks } from "./routes/get-links";
 
 const app = fastify();
 
@@ -26,6 +27,7 @@ app.register(confirmParticipant);
 app.register(createActivity);
 app.register(getActivities);
 app.register(createLink);
+app.register(getLinks);
 
 app.listen({ port: 3333 }, () => {
   console.log("Server running!");
