597c947eb45b7f9e98ff40e5691ace5d0705f165
nielsero <niels.cpp@gmail.com>
2024-07-24 02:45:41 +0200

    feat: Separate activities by day

--- src/routes/get-activities.ts
+++ src/routes/get-activities.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
 import { z } from "zod";
 import { db } from "../lib/db";
+import { dayjs } from "../lib/dayjs";
 
 export async function getActivities(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().get(
@@ -18,14 +19,36 @@ export async function getActivities(app: FastifyInstance) {
 
       const trip = await db.trip.findUnique({
         where: { id: tripId },
-        include: { activities: true },
+        include: {
+          activities: {
+            orderBy: { occurs_at: "asc" },
+          },
+        },
       });
 
       if (!trip) {
         throw new Error("Trip not found.");
       }
 
-      return { activities: trip.activities };
+      const differenceInDaysBetweenTripStartAndEnd = dayjs(trip.ends_at).diff(
+        trip.starts_at,
+        "days"
+      );
+
+      const activities = Array.from({
+        length: differenceInDaysBetweenTripStartAndEnd + 1,
+      }).map((_, index) => {
+        const date = dayjs(trip.starts_at).add(index, "days");
+
+        return {
+          date: date.toDate(),
+          activities: trip.activities.filter((activity) => {
+            return dayjs(activity.occurs_at).isSame(date, "day");
+          }),
+        };
+      });
+
+      return { activities };
     }
   );
 }
