d837a242e96cc80530142eb6356ca5ac5d1f8803
nielsero <niels.cpp@gmail.com>
2024-07-24 15:49:43 +0200

    feat: Update trip details

---
+++ server/src/routes/update-trip.ts
@@ -0,0 +1,54 @@
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod";
+import { db } from "../lib/db";
+import { dayjs } from "../lib/dayjs";
+
+export async function updateTrip(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().put(
+    "/trips/:tripId",
+    {
+      schema: {
+        params: z.object({
+          tripId: z.string().uuid(),
+        }),
+        body: z.object({
+          destination: z.string().min(3),
+          starts_at: z.coerce.date(),
+          ends_at: z.coerce.date(),
+        }),
+      },
+    },
+    async (request) => {
+      const { tripId } = request.params;
+      const { destination, starts_at, ends_at } = request.body;
+
+      const trip = await db.trip.findUnique({
+        where: { id: tripId },
+      });
+
+      if (!trip) {
+        throw new Error("Trip not found.");
+      }
+
+      if (dayjs(starts_at).isBefore(new Date())) {
+        throw new Error("Invalid trip start date");
+      }
+
+      if (dayjs(ends_at).isBefore(starts_at)) {
+        throw new Error("Invalid trip end date");
+      }
+
+      await db.trip.update({
+        where: { id: tripId },
+        data: {
+          destination,
+          starts_at,
+          ends_at,
+        },
+      });
+
+      return { tripId: trip.id };
+    }
+  );
+}
