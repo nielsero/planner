5a738761fd5e96cc210feaffc692083f2ef00dbe
nielsero <niels.cpp@gmail.com>
2024-07-24 03:04:54 +0200

    refactor: Move everything to server folder

---
+++ server/src/routes/confirm-participant.ts
@@ -0,0 +1,43 @@
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod";
+import { db } from "../lib/db";
+
+export async function confirmParticipant(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().get(
+    "/participants/:participantId/confirm",
+    {
+      schema: {
+        params: z.object({
+          participantId: z.string().uuid(),
+        }),
+      },
+    },
+    async (request, reply) => {
+      const { participantId } = request.params;
+
+      const participant = await db.participant.findUnique({
+        where: { id: participantId },
+      });
+
+      if (!participant) {
+        throw new Error("Participant not found.");
+      }
+
+      if (participant.is_confirmed) {
+        return reply.redirect(
+          `http://localhost:3000/trips/${participant.trip_id}`
+        );
+      }
+
+      await db.participant.update({
+        where: { id: participantId },
+        data: { is_confirmed: true },
+      });
+
+      return reply.redirect(
+        `http://localhost:3000/trips/${participant.trip_id}`
+      );
+    }
+  );
+}
---
+++ server/src/routes/create-trip.ts
@@ -0,0 +1,104 @@
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod";
+import { db } from "../lib/db";
+import nodemailer from "nodemailer";
+import { getMailClient } from "../lib/mail";
+import { dayjs } from "../lib/dayjs";
+
+export async function createTrip(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    "/trips",
+    {
+      schema: {
+        body: z.object({
+          destination: z.string().min(3),
+          starts_at: z.coerce.date(),
+          ends_at: z.coerce.date(),
+          owner_name: z.string(),
+          owner_email: z.string().email(),
+          emails_to_invite: z.array(z.string().email()),
+        }),
+      },
+    },
+    async (request) => {
+      const {
+        destination,
+        starts_at,
+        ends_at,
+        owner_name,
+        owner_email,
+        emails_to_invite,
+      } = request.body;
+
+      if (dayjs(starts_at).isBefore(new Date())) {
+        throw new Error("Invalid trip start date");
+      }
+
+      if (dayjs(ends_at).isBefore(starts_at)) {
+        throw new Error("Invalid trip end date");
+      }
+
+      const trip = await db.trip.create({
+        data: {
+          destination,
+          starts_at,
+          ends_at,
+          participants: {
+            createMany: {
+              data: [
+                {
+                  name: owner_name,
+                  email: owner_email,
+                  is_owner: true,
+                  is_confirmed: true,
+                },
+                ...emails_to_invite.map((email) => {
+                  return { email };
+                }),
+              ],
+            },
+          },
+        },
+      });
+
+      const formattedStartDate = dayjs(starts_at).format("LL");
+      const formattedEndDate = dayjs(ends_at).format("LL");
+
+      const confirmationLink = `http://localhost:3333/trips/${trip.id}/confirm`;
+
+      const mail = await getMailClient();
+
+      const message = await mail.sendMail({
+        from: {
+          name: "Equipe Planner",
+          address: "oi@planner.com",
+        },
+        to: {
+          name: owner_name,
+          address: owner_email,
+        },
+        subject: `Confirme sua viagem para ${destination} em ${formattedStartDate}`,
+        html: `
+          <div style="font-family: sans-serif; font-size: 16px; line-height: 1.6">
+            <p>
+              Você solicitou a criação de uma viagem para
+              <strong>${destination}</strong> nas datas de
+              <strong>${formattedStartDate}</strong> até <strong>${formattedEndDate}</strong>.
+            </p>
+            <p></p>
+            <p>Para confirmar sua viagem, clique no link abaixo:</p>
+            <p></p>
+            <p><a href="${confirmationLink}">Confirmar viagem</a></p>
+            <p></p>
+            <p>Caso não saiba do que isso se trata, apenas ignore esse e-mail.</p>
+          </div>
+        `.trim(),
+      });
+
+      console.log(nodemailer.getTestMessageUrl(message));
+
+      return { tripId: trip.id };
+    }
+  );
+}
