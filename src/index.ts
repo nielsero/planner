42664f88a146d24da433e2113f4181331d7fa0fc
nielsero <niels.cpp@gmail.com>
2024-07-24 02:48:42 +0200

    feat: Add create link

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ import { confirmTrip } from "./routes/confirm-trip";
 import { confirmParticipant } from "./routes/confirm-participant";
 import { createActivity } from "./routes/create-activity";
 import { getActivities } from "./routes/get-activities";
+import { createLink } from "./routes/create-link";
 
 const app = fastify();
 
@@ -24,6 +25,7 @@ app.register(confirmTrip);
 app.register(confirmParticipant);
 app.register(createActivity);
 app.register(getActivities);
+app.register(createLink);
 
 app.listen({ port: 3333 }, () => {
   console.log("Server running!");
---
+++ src/routes/create-link.ts
@@ -0,0 +1,43 @@
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod";
+import { db } from "../lib/db";
+
+export async function createLink(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    "/trips/:tripId/links",
+    {
+      schema: {
+        params: z.object({
+          tripId: z.string().uuid(),
+        }),
+        body: z.object({
+          title: z.string().min(3),
+          url: z.string().url(),
+        }),
+      },
+    },
+    async (request) => {
+      const { tripId } = request.params;
+      const { title, url } = request.body;
+
+      const trip = await db.trip.findUnique({
+        where: { id: tripId },
+      });
+
+      if (!trip) {
+        throw new Error("Trip not found.");
+      }
+
+      const link = await db.link.create({
+        data: {
+          title,
+          url,
+          trip_id: tripId,
+        },
+      });
+
+      return { linkId: link.id };
+    }
+  );
+}
