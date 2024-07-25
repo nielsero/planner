8fcb3e4c5d069e78071675cd40a7304df48d19aa
nielsero <niels.cpp@gmail.com>
2024-07-25 19:26:47 +0200

    feat(web): Display destination and dates in trip details page

--- web/src/pages/trip-details/destination-and-date-header.tsx
+++ web/src/pages/trip-details/destination-and-date-header.tsx
@@ -1,18 +1,43 @@
 import { MapPin, Calendar, Settings2 } from "lucide-react";
 import { Button } from "../../components/button";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { api } from "../../lib/api";
+import { format } from "date-fns";
+
+type Trip = {
+  id: string;
+  destination: string;
+  starts_at: string;
+  ends_at: string;
+  is_confirmed: boolean;
+};
 
 export function DestinationAndDateHeader() {
+  const { tripId } = useParams();
+
+  const [trip, setTrip] = useState<Trip | null>(null);
+
+  useEffect(() => {
+    api.get(`/trips/${tripId}`).then((response) => setTrip(response.data.trip));
+  }, [tripId]);
+
+  const displayedDate = trip
+    ? `${format(trip?.starts_at, "d 'de' LLL")} até 
+    ${format(trip?.ends_at, "d 'de' LLL")}`
+    : null;
+
   return (
     <header className="px-4 h-16 rounded-xl bg-zinc-900 shadow-shape flex items-center justify-between">
       <div className="flex items-center gap-2">
         <MapPin className="size-5 text-zinc-400" />
-        <span className="text-zinc-100">Maputo, Moçambique</span>
+        <span className="text-zinc-100">{trip?.destination}</span>
       </div>
 
       <div className="flex items-center gap-5">
         <div className="flex items-center gap-2">
           <Calendar className="size-5 text-zinc-400" />
-          <span className="text-zinc-100">17 a 23 de Agosto</span>
+          <span className="text-zinc-100">{displayedDate}</span>
         </div>
 
         <div className="w-px h-6 bg-zinc-800"></div>
