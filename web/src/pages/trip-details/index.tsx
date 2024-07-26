e08aa9f2a5b7dcd2b713c79c214928b26bb0189f
nielsero <niels.cpp@gmail.com>
2024-07-26 20:14:40 +0200

    refactor(web): Put create & display activities in same component

--- web/src/pages/trip-details/index.tsx
+++ web/src/pages/trip-details/index.tsx
@@ -1,11 +1,9 @@
-import { Plus } from "lucide-react";
 import { useState } from "react";
 import { CreateActivityModal } from "./create-activity-modal";
 import { ImportantLinks } from "./important-links";
 import { Guests } from "./guests";
 import { Activities } from "./activities";
 import { DestinationAndDateHeader } from "./destination-and-date-header";
-import { Button } from "../../components/button";
 import { CreateLinkModal } from "./create-link-modal";
 
 export function TripDetailsPage() {
@@ -35,23 +33,12 @@ export function TripDetailsPage() {
 
       <main className="flex gap-16 px-4">
         <div className="flex-1 space-y-6">
-          <div className="flex items-center justify-between">
-            <h2 className="text-3xl font-semibold">Actividades</h2>
-
-            <Button onClick={openCreateActivityModal}>
-              <Plus className="size-5" />
-              Cadastrar actividade
-            </Button>
-          </div>
-
-          <Activities />
+          <Activities openCreateActivityModal={openCreateActivityModal} />
         </div>
 
         <div className="w-80 space-y-6">
           <ImportantLinks openCreateLinkModal={openCreateLinkModal} />
-
           <div className="w-full h-px bg-zinc-800"></div>
-
           <Guests />
         </div>
       </main>
