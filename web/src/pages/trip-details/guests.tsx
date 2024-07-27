e2dcd679a9437e9383695f68c10eb6e9dd927a6e
nielsero <niels.cpp@gmail.com>
2024-07-27 22:35:17 +0200

    feat(web): Create invite guest modal

--- web/src/pages/trip-details/guests.tsx
+++ web/src/pages/trip-details/guests.tsx
@@ -1,16 +1,32 @@
-import { CheckCircle2, CircleDashed, UserCog } from "lucide-react";
+import { CheckCircle2, CircleDashed, Mail, UserCog } from "lucide-react";
 import { Button } from "../../components/button";
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import { getParticipants } from "../../api/get-participants";
+import { FormEvent, useState } from "react";
+import { Modal } from "../../components/modal";
 
 export function Guests() {
   const { tripId } = useParams() as { tripId: string };
+
+  const [isInviteGuestModalOpen, setIsInviteGuestModalOpen] = useState(false);
 
   const { data } = useQuery({
     queryKey: ["participants", tripId],
     queryFn: () => getParticipants({ tripId }),
   });
+
+  function openInviteGuestModal() {
+    setIsInviteGuestModalOpen(true);
+  }
+
+  function closeInviteGuestModal() {
+    setIsInviteGuestModalOpen(false);
+  }
+
+  function handleInviteGuest(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+  }
 
   return (
     <div className="space-y-6">
@@ -42,10 +58,39 @@ export function Guests() {
         })}
       </div>
 
-      <Button variant="secondary" className="w-full h-11">
+      <Button
+        variant="secondary"
+        className="w-full h-11"
+        onClick={openInviteGuestModal}
+      >
         <UserCog className="size-5" />
-        Adicionar convidados
+        Adicionar convidado
       </Button>
+
+      {isInviteGuestModalOpen && (
+        <Modal
+          title="Adicionar convidado"
+          description="O convidado irá receber e-mail para confirmar a participação na viagem."
+          onClose={closeInviteGuestModal}
+          className="w-[640px]"
+        >
+          <form className="space-y-3" onSubmit={handleInviteGuest}>
+            <div className="h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
+              <Mail className="size-5 text-zinc-400" />
+              <input
+                className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+                type="email"
+                name="email"
+                placeholder="E-mail do convidado"
+              />
+            </div>
+
+            <Button type="submit" className="w-full h-11">
+              Adicionar convidado
+            </Button>
+          </form>
+        </Modal>
+      )}
     </div>
   );
 }
