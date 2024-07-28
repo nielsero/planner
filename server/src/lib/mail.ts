bf3a70d06c96987a2bef9b3ca9d49dd0e5f53c57
nielsero <niels.cpp@gmail.com>
2024-07-28 20:07:38 +0200

    feat: Send emails in production

--- server/src/lib/mail.ts
+++ server/src/lib/mail.ts
@@ -1,9 +1,20 @@
 import nodemailer from "nodemailer";
+import { env } from "../env";
 
 export async function getMailClient() {
+  if (env.NODE_ENV === "production" && env.EMAIL_USER && env.EMAIL_PASS) {
+    return nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: env.EMAIL_USER,
+        pass: env.EMAIL_PASS,
+      },
+    });
+  }
+
   const account = await nodemailer.createTestAccount();
 
-  const transporter = nodemailer.createTransport({
+  return nodemailer.createTransport({
     host: "smtp.ethereal.email",
     port: 587,
     secure: false,
@@ -12,6 +23,4 @@ export async function getMailClient() {
       pass: account.pass,
     },
   });
-
-  return transporter;
 }
