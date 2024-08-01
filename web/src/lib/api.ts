b2500b8318088dbd60be817186bb5f53f22312d1
nielsero <niels.cpp@gmail.com>
2024-08-01 22:21:23 +0200

    feat(web): Validate env variables

--- web/src/lib/api.ts
+++ web/src/lib/api.ts
@@ -1,4 +1,6 @@
 import axios from "axios";
+import { env } from "../env";
+
 export const api = axios.create({
-  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`,
+  baseURL: `${env.VITE_API_BASE_URL}/api`,
 });
